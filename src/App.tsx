776143611860c12d6bde31a60e5e2f2452f57f68
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-28 17:51:53 +0200

    Add AppBar with Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import AppBar from './components/layout/Header/AppBar';
+import Container from '@mui/material/Container';
 const App = () => {
-  return <div className='App'>Test</div>;
+  return (
+    <div className='App'>
+      <Container>
+        <AppBar />
+      </Container>
+    </div>
+  );
 };
 
 export default App;
